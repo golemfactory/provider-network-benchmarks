670a2f38fb484e201c9784bd3d25e6d2c1ab2922
Przemyslaw Walski <pwalski@users.noreply.github.com>
2022-10-03 22:49:35 +0200

    Chainlink example WIP (parallel)

--- chainlink-example/chainlink_request.py
+++ chainlink-example/chainlink_request.py
@@ -1,64 +1,112 @@
 #!/usr/bin/env python3
-# countasync.py
+# chainlink_request.py
 
 import asyncio
+import concurrent.futures
+from multiprocessing import cpu_count
 import argparse
 from web3 import Web3
 import time
 import sys
 
-async def chainlink_request(iteration: int, batch_iteration: int):
+NUM_CORES = cpu_count()
+
+# AggregatorV3Interface ABI
+GLM_ABI = '\
+    [{\
+        "inputs":[],\
+        "name":"decimals",\
+        "outputs":[{"internalType":"uint8","name":"","type":"uint8"}],\
+        "stateMutability":"view",\
+        "type":"function"\
+    },{\
+        "inputs":[],\
+        "name":"description",\
+        "outputs":[{"internalType":"string","name":"","type":"string"}],\
+        "stateMutability":"view",\
+        "type":"function"\
+    },{\
+        "inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],\
+        "name":"getRoundData",\
+        "outputs":[\
+            {"internalType":"uint80","name":"roundId","type":"uint80"},\
+            {"internalType":"int256","name":"answer","type":"int256"},\
+            {"internalType":"uint256","name":"startedAt","type":"uint256"},\
+            {"internalType":"uint256","name":"updatedAt","type":"uint256"},\
+            {"internalType":"uint80","name":"answeredInRound","type":"uint80"}\
+        ],\
+        "stateMutability":"view",\
+        "type":"function"\
+    },{\
+        "inputs":[],\
+        "name":"latestRoundData",\
+        "outputs":[\
+            {"internalType":"uint80","name":"roundId","type":"uint80"},\
+            {"internalType":"int256","name":"answer","type":"int256"},\
+            {"internalType":"uint256","name":"startedAt","type":"uint256"},\
+            {"internalType":"uint256","name":"updatedAt","type":"uint256"},\
+            {"internalType":"uint80","name":"answeredInRound","type":"uint80"}\
+        ],\
+        "stateMutability":"view",\
+        "type":"function"\
+    },{\
+        "inputs":[],\
+        "name":"version",\
+        "outputs":[{"internalType":"uint256","name":"","type":"uint256"}],\
+        "stateMutability":"view",\
+        "type":"function"\
+    }]'
+
+def chainlink_request(iteration: int, batch_iteration: int):
+    request_time = time.time_ns()
+
     # Change this to use your own infura ID
     web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
-    # AggregatorV3Interface ABI
-    abi = '[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
     # Price Feed address
     addr = '0x83441C3A10F4D05de6e0f2E849A850Ccf27E6fa7'
 
     # Set up contract instance
-    contract = web3.eth.contract(address=addr, abi=abi)
+    contract = web3.eth.contract(address=addr, abi=GLM_ABI)
     # Make call to latestRoundData()
     latestData = contract.functions.latestRoundData().call()
-    f"{iteration}.{batch_iteration}:\t{latestData}"
-
-
-async def requests(batch: int, iteration: int):
-    # loop = asyncio.get_event_loop()
-    tasks = []
-    for i in range(batch):
-        task = asyncio.create_task(chainlink_request(iteration, i))
-        tasks.append(task)
-    results = await asyncio.gather(*tasks)
-    print(results)
-    # group = asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
-    # await loop.run_until_complete(group)
-
-async def request_batches(batch: int, iterations: int):
-    global_time = time.time_ns()
-
-    for i in range(iterations):
-        batch_time = time.time_ns()
-        print(f"Iteration #{i}.")
-
-        results = asyncio.gather(requests(batch, i))
-        await asyncio.sleep(1)
-        print(results)
-        batch_time = time.time_ns() - batch_time
-        print(f"Iteration #{i} took: {batch_time} ns.")
+    # accessing 'answer' field
+    golem_usd_price = latestData[1] / 100_000_000
     
-    global_time = time.time_ns() - global_time
-    print(f"All took: {global_time} ns.")
+    request_time = (time.time_ns() - request_time) / 1_000_000
+    print(f"{iteration}.{batch_iteration}:\t{request_time} ms\t GLM/USD {golem_usd_price}")
+
+async def batch(executor: concurrent.futures.ThreadPoolExecutor, batch_size: int, iteration: int):
+    for i in range(batch_size):
+        new_future = executor.submit(
+            chainlink_request, 
+            iteration=iteration, 
+            batch_iteration=i
+        )
+
+async def batches(threads_size: int, batch_size: int, iterations_size: int):
+    with concurrent.futures.ThreadPoolExecutor(threads_size) as executor:
+        for i in range(iterations_size):
+            print(f"{i}:")
+            results = await batch(executor, batch_size, i)
+            await asyncio.sleep(1)
+        
+        executor.shutdown(wait=True, cancel_futures=False)
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--batch", default=100, type=int, help="Batch size")
     parser.add_argument("-i", "--iterations", default=5, type=int, help="Number of iterations")
+    parser.add_argument("-t", "--threads", default=-1, type=int, help="Number of threads")
     args = parser.parse_args()
     
-    batch = args.batch
-    iterations = args.iterations
+    batch_size = args.batch
+    iterations_size = args.iterations
+    threads_size = NUM_CORES
+    if args.threads > 0:
+        threads_size = args.threads
+
     
-    await request_batches(batch, iterations)
+    await batches(threads_size, batch_size, iterations_size)
 
 if __name__ == "__main__":
     asyncio.run(main())
