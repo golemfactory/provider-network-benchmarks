279029711c250538a1bfbe440ec8bcd09266c3a7
Przemyslaw Walski <pwalski@users.noreply.github.com>
2022-10-04 00:48:57 +0200

    Chainlink example WIP (as Class)

--- chainlink-example/chainlink_request.py
+++ chainlink-example/chainlink_request.py
@@ -57,40 +57,47 @@ GLM_ABI = '\
         "type":"function"\
     }]'
 
-def chainlink_request(iteration: int, batch_iteration: int):
-    request_time = time.time_ns()
+class Request:
+    def __init__(self, threads, batch_size, iterations, rpc):
+        self.threads_size = threads
+        self.batch_size = batch_size
+        self.iterations = iterations
+        self.rpc = rpc
 
-    # Change this to use your own infura ID
-    web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
-    # Price Feed address
-    addr = '0x83441C3A10F4D05de6e0f2E849A850Ccf27E6fa7'
-
-    # Set up contract instance
-    contract = web3.eth.contract(address=addr, abi=GLM_ABI)
-    # Make call to latestRoundData()
-    latestData = contract.functions.latestRoundData().call()
-    # accessing 'answer' field
-    golem_usd_price = latestData[1] / 100_000_000
     
-    request_time = (time.time_ns() - request_time) / 1_000_000
-    print(f"{iteration}.{batch_iteration}:\t{request_time} ms\t GLM/USD {golem_usd_price}")
+    def chainlink_request(self, iteration: int, batch_iteration: int):
+        request_time = time.time_ns()
 
-async def batch(executor: concurrent.futures.ThreadPoolExecutor, batch_size: int, iteration: int):
-    for i in range(batch_size):
-        new_future = executor.submit(
-            chainlink_request, 
-            iteration=iteration, 
-            batch_iteration=i
-        )
+        # Change this to use your own infura ID
+        web3 = Web3(Web3.HTTPProvider(self.rpc))
+        # Price Feed address
+        addr = '0x83441C3A10F4D05de6e0f2E849A850Ccf27E6fa7'
 
-async def batches(threads_size: int, batch_size: int, iterations_size: int):
-    with concurrent.futures.ThreadPoolExecutor(threads_size) as executor:
-        for i in range(iterations_size):
-            print(f"{i}:")
-            results = await batch(executor, batch_size, i)
-            await asyncio.sleep(1)
+        # Set up contract instance
+        contract = web3.eth.contract(address=addr, abi=GLM_ABI)
+        # Make call to latestRoundData()
+        latestData = contract.functions.latestRoundData().call()
+        # accessing 'answer' field
+        golem_usd_price = latestData[1] / 100_000_000
         
-        executor.shutdown(wait=True, cancel_futures=False)
+        request_time = (time.time_ns() - request_time) / 1_000_000
+        print(f"{iteration}.{batch_iteration}:\t{request_time}ms\t GLM/USD {golem_usd_price}")
+
+    async def batch(self, executor: concurrent.futures.ThreadPoolExecutor, iteration: int):
+        for i in range(self.batch_size):
+            new_future = executor.submit(
+                self.chainlink_request,
+                iteration=iteration,
+                batch_iteration=i
+            )
+
+    async def run(self):
+        with concurrent.futures.ThreadPoolExecutor(self.threads_size) as executor:
+            for i in range(self.iterations):
+                print(f"{i}:")
+                await self.batch(executor, i)
+                await asyncio.sleep(1)
+            executor.shutdown(wait=True, cancel_futures=False)
 
 async def main():
     parser = argparse.ArgumentParser()
@@ -104,9 +111,8 @@ async def main():
     threads_size = NUM_CORES
     if args.threads > 0:
         threads_size = args.threads
-
     
-    await batches(threads_size, batch_size, iterations_size)
+    await Request(threads_size, batch_size, iterations_size, 'https://rpc.ankr.com/eth').run()
 
 if __name__ == "__main__":
     asyncio.run(main())
