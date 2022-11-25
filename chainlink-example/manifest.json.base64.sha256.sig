c90ddf6f96b08da580d61b72593a34d180bb9ef5
Przemyslaw Walski <pwalski@users.noreply.github.com>
2022-11-25 10:22:01 +0100

    Golem Examples certificate + sig

---
+++ chainlink-example/manifest.json.base64.sha256.sig
@@ -0,0 +1,2 @@
+N;h*Ù¹bgÑ1j9¢LC&½Ûºíì\´<N‘â‹ã€øÏ¿ŽýHOo©`¾Hª”F3%»‰Ž{ SÞÉÛ{MM-6ÞCÒ°šª•& ’¯§Ý_¿¬ƒÊVÏg4ŽÊì&RÝL¢r¯£U=Â*ùã¯Èç",´m¿£ÿyßÚ|ö²
+NÝY¨'¤L‡à Ï®©7QKPú”ç4ÍP¦ÔòÞa9~p™^ïò‚Å HáÏ{dPhˆ.ûX>qð½–µ²¹ë\AÔ*q¦-”üÚËµš-"ÒuâR…Zež/¥„;±A7ÿ¦3Hóªô%ƒ/.úºû˜]p
--- chainlink-example/sign.sh
+++ chainlink-example/sign.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]; then
-  echo 1>&2 "$0: Add path to private key and app author's certificate as params"
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: Add path to private key as a param"
   exit 2
 fi
 
 PRIVATE_KEY=$1
-APP_AUTHOR_CERT=$2
 base64 manifest.json --wrap=0 > manifest.json.base64
-openssl dgst -sha256 -sign $PRIVATE_KEY -out manifest.json.base64.sign.sha256 manifest.json.base64
-base64 manifest.json.base64.sign.sha256 --wrap=0 > manifest.json.base64.sign.sha256.base64
-base64 $2 --wrap=0 > author.crt.pem.base64
+openssl dgst -sha256 -sign $PRIVATE_KEY -out manifest.json.base64.sha256.sig manifest.json.base64
+base64 manifest.json.base64.sha256.sig --wrap=0 > manifest.json.base64.sha256.sig.base64
+base64 examples.crt.chain.pem --wrap=0 > examples.crt.chain.pem.base64
